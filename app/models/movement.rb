57b8e6d141338d7f9e08dba98c8eb7757ba26cc5
Fatima Talavera <fa.talavera95@gmail.com>
2016-06-02 20:56:05 -0400

    Mas cambios en la vista de movimientos

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -2,6 +2,10 @@ class Movement < ActiveRecord::Base
   belongs_to :motive
   belongs_to :person
   has_many :d_movements, dependent: :destroy
+
+  delegate :nombre, to: :person, prefix: true, allow_nil: true
+  delegate :descripcion, to: :motive, prefix: true, allow_nil: true
+
   accepts_nested_attributes_for :d_movements, :allow_destroy => true
 
   validates :fecha, :presence => {:message => "seleccione una fecha"}
