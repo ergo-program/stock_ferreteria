e245bc93809c6ad6386e65bcc0e5212026b850e8
Fatima Talavera <fa.talavera95@gmail.com>
2016-06-05 18:35:30 -0400

    Correccion en callback movimientos

--- app/models/d_movement.rb
+++ app/models/d_movement.rb
@@ -7,22 +7,13 @@ class DMovement < ActiveRecord::Base
   						:numericality => {:greater_than => 0, :message => "debe ser un numero positivo"}
   validate :stock_negativo
 
-  after_create :ac_movement
-
 
 
   protected
 
-  #una vez creado actualiza el stock del producto
-  def ac_movement
-  	product = Product.find(self.product_id)
-  	self.precio = product.precio_venta
-  	Product.update(product, :cantidad => (product.cantidad - self.cantidad))
-  end
-
   #controla que haya stock suficiente
   def stock_negativo
-    errors.add(:cantidad, "la cantidad supera el stock exixtente") if Product.find(product_id).cantidad < cantidad
+    errors.add(:cantidad, "supera el stock exixtente") if Product.find(product_id).cantidad < cantidad
   end
 
 end
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -7,7 +7,9 @@ class Movement < ActiveRecord::Base
   delegate :descripcion, to: :motive, prefix: true, allow_nil: true
 
   accepts_nested_attributes_for :d_movements, :allow_destroy => true
-
+ 
+  after_create :ac_movement
+  
   validates :fecha, :presence => {:message => "seleccione una fecha"}
   validates :motive_id, :presence => {:message => "seleccione un motivo"}
 
@@ -21,6 +23,18 @@ class Movement < ActiveRecord::Base
 
   def detalle_vacio?
     d_movements.empty?
+  end
+
+  
+
+  #una vez creado actualiza el stock del producto
+  def ac_movement
+    self.d_movements.each do |child|
+      product = Product.find(child.product_id)
+      DMovement.update(child, :precio => product.precio_venta)
+      cantidad = product.cantidad - child.cantidad
+      Product.update(product, :cantidad => cantidad)
+    end
   end
   
 end
